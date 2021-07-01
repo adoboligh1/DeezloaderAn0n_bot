0228025bcc548c8288efc22794b4e012f3a1f400
9_Mad-Max_5 <maximilian.huebner95@gmx.de>
2021-07-01 16:12:06 +0200

    fix

--- dwsongs-normal.py
+++ dwsongs-normal.py
@@ -3,6 +3,7 @@
 import os
 import logging
 import dwytsongs
+import soundscrape
 from utils import *
 from settings import *
 from time import sleep
@@ -33,6 +34,7 @@ from telegram import (
     InlineKeyboardMarkup, TelegramError, InputMediaPhoto,
     InlineQueryResultArticle, InputTextMessageContent
 )
+from deezloader.deezloader.bin.deez-dw import link
 
 config = ConfigParser()
 config.read(ini_file)
@@ -358,7 +360,7 @@ def track(link, chat_id, quality):
                     #os.system("cd /youtube && spotdl %s" % link)
                     #os.system('folder=$(ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 youtube/*.mp3) && Songs/"$folder"/ && mv youtube/*.mp3 Songs/"$folder"/')
                 elif "deezer" in link:
-                    #sendMessage(
+                    # sendMessage(
                     #    chat_id, "YouTube-download only for Spotify Links supported! Please pas a YouTube-link")
                     mode = dwytsongs.download_trackdee
 
@@ -776,6 +778,10 @@ def Link(link, chat_id, quality, message_id):
                 )
             else:
                 sendMessage(chat_id, not_supported_links % link)
+
+        elif "soundcloud" in link:
+            # soundscrape(link)
+            sendMessage(chat_id, "Im new and don#t talk to much!")
 
         else:
             sendMessage(chat_id, not_supported_links % link)
